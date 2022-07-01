28ee6ce6b8b763fa77df96d775d79a44fd03bc8c
Serhii <kopytchukserhii@gmail.com>
2022-07-01 13:02:32 +0300

    channelMessagingViewModel refactored

--- Chat/ViewModel/ChannelMessagingViewModel.swift
+++ Chat/ViewModel/ChannelMessagingViewModel.swift
@@ -25,27 +25,38 @@ class ChannelMessagingViewModel: ObservableObject {
         var messages: [Message] = []
         dataBase.collection("channels").document(self.currentChannel.id ?? "someId").collection("messages")
             .addSnapshotListener { querySnapshot, error in
-            guard let documents = querySnapshot?.documents else {
-                print("Error fetching documets: \(String(describing: error))")
-                return
-            }
-            self.currentChannel.messages = documents.compactMap { document -> Message? in
-                do {
-                    messages.append(try document.data(as: Message.self))
-                    return  messages.last
-                } catch {
-                    print("error deconding documet into Message: \(error)")
-                    return nil
+                
+                guard let documents = querySnapshot?.documents else {
+                    print("Error fetching documets: \(String(describing: error))")
+                    return
                 }
-            }
-            self.currentChannel.messages?.sort { $0.timestamp < $1.timestamp}
-            messages.sort {$0.timestamp < $1.timestamp }
-                DispatchQueue.main.async {
-                    competition(messages)
-                }
-                return
+
+                self.currentChannel.messages = self.documentsToMessages(messages: &messages, documents: documents)
+
+                self.sortMessages(messages: &messages)
+
+                competition(messages)
+
             }
     }
+
+    private func documentsToMessages(messages: inout [Message], documents: [QueryDocumentSnapshot]) -> [Message] {
+        return documents.compactMap { document -> Message? in
+            do {
+                messages.append(try document.data(as: Message.self))
+                return  messages.last
+            } catch {
+                print("error deconding documet into Message: \(error)")
+                return nil
+            }
+        }
+    }
+
+    private func sortMessages( messages: inout [Message]) {
+        self.currentChannel.messages?.sort { $0.timestamp < $1.timestamp}
+        messages.sort {$0.timestamp < $1.timestamp }
+    }
+
 
     func sendMessage(text: String) {
         let newMessage = Message(id: "\(UUID())", text: text, senderId: self.currentUser.id, timestamp: Date())
