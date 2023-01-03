86b67ecb710b6e95d76ffe1533b44871b299c773
Serhii <kopytchukserhii@gmail.com>
2023-01-03 18:24:20 +0200

    launch screen animation

--- Chat/View/Main/RootView.swift
+++ Chat/View/Main/RootView.swift
@@ -12,13 +12,17 @@ import Firebase
 
 // SF Symbols
 struct RootView: View {
-
+    
     // MARK: - vars
+    
+    @State private var launchScreenOpacity: CGFloat = 1
+    @State private var launchScreenIconScale: CGFloat = 1
+    
     @EnvironmentObject var viewModel: UserViewModel
-
+    
     // MARK: - body
     var body: some View {
-
+        
         NavigationStack {
             VStack {
                 if viewModel.signedIn {
@@ -30,8 +34,32 @@ struct RootView: View {
             .ignoresSafeArea(.all, edges: .bottom)
             .navigationBarBackButtonHidden(true)
         }
+        .overlay(alignment: .center, content: {
+            ZStack {
+                
+                Color.background
+                    .ignoresSafeArea()
+                
+                Image("icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 128, height: 128)
+                    .scaleEffect(launchScreenIconScale, anchor: .center)
+                
+            }
+            .ignoresSafeArea()
+            .opacity(launchScreenOpacity)
+        })
         .onAppear {
             viewModel.signedIn = viewModel.isSignedIn
+            
+            withAnimation(.easeOut) {
+                launchScreenIconScale = 0.8
+            }
+            
+            withAnimation(.easeOut(duration: 0.5).delay(0.5)) {
+                launchScreenOpacity = 0
+            }
         }
     }
 }
