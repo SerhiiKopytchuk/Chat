78a8f937480ab50c6cd90479f37c2bca25124ff4
Serhii <kopytchukserhii@gmail.com>
2023-01-13 18:08:42 +0200

    extracted read size extension

--- Chat/Extensions/View + Extenston.swift
+++ Chat/Extensions/View + Extenston.swift
@@ -103,4 +103,13 @@ extension View {
             }
     }
 
+    func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
+        background(
+            GeometryReader { geometryProxy in
+                Color.clear
+                    .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
+            }
+        )
+        .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
+    }
 }
