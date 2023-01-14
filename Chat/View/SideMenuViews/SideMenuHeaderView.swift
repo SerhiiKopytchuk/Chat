de05f6dc198265f527fd6fdad4b3111e591727e6
Serhii <kopytchukserhii@gmail.com>
2023-01-14 18:55:30 +0200

    refactored side menu

--- Chat/View/SideMenuViews/SideMenuHeaderView.swift
+++ Chat/View/SideMenuViews/SideMenuHeaderView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import FirebaseStorage
 import FirebaseAuth
 import SDWebImageSwiftUI
+
 struct SideMenuHeaderView: View {
     // MARK: - vars
     @Binding var isShowingSideMenu: Bool
@@ -16,14 +17,10 @@ struct SideMenuHeaderView: View {
 
     // MARK: image properties
     @State private var myImageUrl = URL(string: "")
-    @State private var isFindUserImage = true
     private let imageSize: CGFloat = 65
 
     // MARK: - body
     var body: some View {
-
-        ZStack(alignment: .topTrailing) {
-            closeSideMenuButton
 
             VStack(alignment: .leading) {
 
@@ -34,30 +31,18 @@ struct SideMenuHeaderView: View {
 
                 Text(userViewModel.currentUser.gmail)
                     .font(.system(size: 14 ))
-                    .padding(.bottom, 24)
-
-                // MARK: chats and channels label
-                HStack {
-                    HStack {
-                        Text("\(userViewModel.currentUser.chats.count)").bold()
-                        Text(userViewModel.currentUser.chats.count == 1 ? "Chat" : "Chats")
-                    }
-                    HStack {
-                        Text("\(userViewModel.currentUser.channels.count)").bold()
-                        Text(userViewModel.currentUser.channels.count == 1 ? "Channel" : "Channels")
-                    }
-                    Spacer()
-                }
-                Spacer()
             }
             .padding()
-        }
     }
 
     // MARK: - viewBuilders
     @ViewBuilder private var userImage: some View {
-        if isFindUserImage {
             WebImage(url: myImageUrl)
+                .placeholder(content: {
+                    EmptyImageWithCharacterView(text: userViewModel.currentUser.name,
+                                                colour: userViewModel.currentUser.colour,
+                                                size: imageSize)
+                })
                 .resizable()
                 .scaledToFill()
                 .clipped()
@@ -69,36 +54,18 @@ struct SideMenuHeaderView: View {
                     imageStartSetup()
                 }
                 .accessibilityValue("notEmptyImage")
-        } else {
-            EmptyImageWithCharacterView(text: userViewModel.currentUser.name,
-                                        colour: userViewModel.currentUser.colour,
-                                        size: imageSize)
-                .padding(.bottom, 16)
-        }
-    }
-
-    @ViewBuilder private var closeSideMenuButton: some View {
-        Button {
-            withAnimation(.spring()) {
-                isShowingSideMenu.toggle()
-            }
-        } label: {
-            Image(systemName: "xmark")
-                .frame(width: 32, height: 32)
-                .padding()
-        }
     }
 
     // MARK: - functions
     private func imageStartSetup() {
         let ref = StorageReferencesManager.shared.getProfileImageReference(userId: userViewModel.currentUser.id)
-        ref.downloadURL { url, err in
-            if err != nil {
-                self.isFindUserImage = false
-                return
-            }
-            withAnimation(.easeInOut) {
-                self.myImageUrl = url
+        DispatchQueue.global(qos: .utility).async {
+            ref.downloadURL { url, err in
+                DispatchQueue.main.async {
+                    withAnimation(.easeOut) {
+                        self.myImageUrl = url
+                    }
+                }
             }
         }
     }
