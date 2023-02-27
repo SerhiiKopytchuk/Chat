101c825e24fe83385bc2a505ba38d2afc98de2f1
Serhii <kopytchukserhii@gmail.com>
2023-02-27 18:11:26 +0200

    remaned extensions and changed destinations

--- Chat/Extensions/UIApplication + Extension.swift
+++ Chat/Extensions/UIApplication + Extension.swift
@@ -1,5 +1,5 @@
 //
-//  UIAplication + Extension.swift
+//  UIApplication + Extension.swift
 //  Chat
 //
 //  Created by Serhii Kopytchuk on 25.08.2022.
@@ -8,6 +8,7 @@
 import SwiftUI
 
 extension UIApplication {
+    /// hide keyboard
     func endEditing() {
         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
