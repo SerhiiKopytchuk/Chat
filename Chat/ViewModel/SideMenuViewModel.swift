c6458f8cfe956aed0b4b7f70efe69239540550b9
Serhii <kopytchukserhii@gmail.com>
2023-03-12 12:24:49 +0200

    removed subscription page

--- Chat/ViewModel/SideMenuViewModel.swift
+++ Chat/ViewModel/SideMenuViewModel.swift
@@ -11,7 +11,6 @@ enum SideMenuViewModel: Int, CaseIterable {
     case profile
     case createChannel
     case search
-    case subscriptions
     case logout
 
     var title: String {
@@ -19,7 +18,6 @@ enum SideMenuViewModel: Int, CaseIterable {
         case .profile: return "Profile"
         case .createChannel: return "Create channel"
         case .search: return "Search"
-        case .subscriptions: return "Subscriptions"
         case .logout: return "Logout"
         }
     }
@@ -29,7 +27,6 @@ enum SideMenuViewModel: Int, CaseIterable {
         case .profile: return "person"
         case .createChannel: return "plus"
         case .search: return "magnifyingglass"
-        case .subscriptions: return "star"
         case .logout: return "arrow.left.square"
         }
     }
